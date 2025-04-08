46328ec9c48b101fec5e1b1e83b65cff18b4ee3a
Just <grivten@yandex.ru>
2025-04-08 21:39:52 +0300

    equippable component

--- src/main/java/net/just_s/mixin/SmithingTransformRecipeMixin.java
+++ src/main/java/net/just_s/mixin/SmithingTransformRecipeMixin.java
@@ -4,18 +4,25 @@ import net.minecraft.component.ComponentChanges;
 import net.minecraft.component.ComponentMap;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.CustomModelDataComponent;
+import net.minecraft.component.type.EquippableComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.equipment.EquipmentAssetKeys;
 import net.minecraft.recipe.SmithingTransformRecipe;
 import net.minecraft.recipe.input.SmithingRecipeInput;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mixin(SmithingTransformRecipe.class)
 public class SmithingTransformRecipeMixin {
@@ -37,24 +44,56 @@ public class SmithingTransformRecipeMixin {
 		if (customName != null) {
 			// Has CustomName -> Adding CustomItemModel
 			String customNameAsString = customName.getString();
-
-			CustomModelDataComponent modelComponent = new CustomModelDataComponent(
-					List.of(), 					// Float
-					List.of(), 					// Boolean
-					List.of(customNameAsString),// String
-					List.of()					// Integer
-			);
-			componentChanges = ComponentChanges.builder()
-					.add(DataComponentTypes.CUSTOM_MODEL_DATA, modelComponent)
-					.build();
+			componentChanges = modifyComponentChanges(returnableStack, customNameAsString);
 		} else {
 			// Does not have CustomName -> Removing CustomItemModel
-			componentChanges = ComponentChanges.builder()
-					.remove(DataComponentTypes.CUSTOM_MODEL_DATA)
-					.build();
+			componentChanges = resetComponentChanges(returnableStack);
 		}
 
 		returnableStack.applyChanges(componentChanges);
 		cir.setReturnValue(returnableStack);
+	}
+
+	@Unique
+	private ComponentChanges modifyComponentChanges(ItemStack item, String customModelDataString) {
+		CustomModelDataComponent modelComponent = new CustomModelDataComponent(
+				List.of(), 						// Float
+				List.of(), 						// Boolean
+				List.of(customModelDataString),	// String
+				List.of()						// Integer
+		);
+		ComponentChanges.Builder componentBuilder = ComponentChanges.builder();
+		componentBuilder = componentBuilder.add(DataComponentTypes.CUSTOM_MODEL_DATA, modelComponent);
+
+		if (item.getDefaultComponents().contains(DataComponentTypes.EQUIPPABLE)) {
+			ComponentMap componentMapReturnable = item.getComponents();
+			EquippableComponent returnableEquippableComponent = componentMapReturnable.get(DataComponentTypes.EQUIPPABLE);
+
+			EquippableComponent newEquippableComponent = new EquippableComponent(
+					returnableEquippableComponent.slot(),
+					returnableEquippableComponent.equipSound(),
+					Optional.of(RegistryKey.of(EquipmentAssetKeys.REGISTRY_KEY, Identifier.of(customModelDataString))),
+					returnableEquippableComponent.cameraOverlay(),
+					returnableEquippableComponent.allowedEntities(),
+					returnableEquippableComponent.dispensable(),
+					returnableEquippableComponent.swappable(),
+					returnableEquippableComponent.damageOnHurt()
+			);
+			componentBuilder.add(DataComponentTypes.EQUIPPABLE, newEquippableComponent);
+		}
+		return componentBuilder.build();
+	}
+
+	@Unique
+	private ComponentChanges resetComponentChanges(ItemStack item) {
+		ComponentChanges.Builder componentBuilder = ComponentChanges.builder();
+		componentBuilder = componentBuilder.remove(DataComponentTypes.CUSTOM_MODEL_DATA);
+
+		if (item.getDefaultComponents().contains(DataComponentTypes.EQUIPPABLE)) {
+			EquippableComponent newEquippableComponent = item.getDefaultComponents().get(DataComponentTypes.EQUIPPABLE);
+			componentBuilder.add(DataComponentTypes.EQUIPPABLE, newEquippableComponent);
+		}
+
+		return componentBuilder.build();
 	}
 }
