433b5a87dc9bb3161fa7fc682910c89930094430
Mynam <mynameisiphone10@gmail.com>
2025-04-15 21:08:14 +0500

    Make custom model recipe a different recipe type Add asset_id validation to prevent errors

--- src/main/java/net/just_s/CIMMod.java
+++ src/main/java/net/just_s/CIMMod.java
@@ -1,6 +1,9 @@
 package net.just_s;
 
 import net.fabricmc.api.ModInitializer;
+import net.just_s.recipe.ModRecipes;
+import net.just_s.recipe.ingredient.ModIngredients;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +13,12 @@ public class CIMMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModIngredients.init();
+		ModRecipes.init();
 		LOGGER.info("Custom Item Models loaded!");
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
 	}
 }
--- src/main/java/net/just_s/datagen/CIMDataGenerator.java
+++ src/main/java/net/just_s/datagen/CIMDataGenerator.java
@@ -4,9 +4,9 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class CIMDataGenerator implements DataGeneratorEntrypoint {
-    @Override
-    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-        pack.addProvider(SmithingModelRecipeProvider::new);
-    }
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(SmithingModelRecipeProvider::new);
+	}
 }
