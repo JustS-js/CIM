3350c6c32e171e738aa03f9fb5daa3dfd3106fe5
DimasKama <mynameisiphone10@gmail.com>
2026-03-26 11:00:40 +0500

    Migrate to mojmaps

--- src/main/java/net/just_s/recipe/ModRecipes.java
+++ src/main/java/net/just_s/recipe/ModRecipes.java
@@ -1,20 +1,20 @@
 package net.just_s.recipe;
 
 import net.just_s.CIMMod;
-import net.minecraft.network.codec.PacketCodec;
-import net.minecraft.recipe.RecipeSerializer;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.world.item.crafting.RecipeSerializer;
 
 public class ModRecipes {
 	public static final RecipeSerializer<CustomItemModelSmithingRecipe> CUSTOM_ITEM_MODEL_SMITHING_SERIALIZER = new CustomItemModelSmithingRecipe.Serializer();
 
 	public static void init() {
-		Registry.register(Registries.RECIPE_SERIALIZER, CIMMod.id("smithing"), CUSTOM_ITEM_MODEL_SMITHING_SERIALIZER);
+		Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, CIMMod.id("smithing"), CUSTOM_ITEM_MODEL_SMITHING_SERIALIZER);
 	}
 
-	public static <B, T> PacketCodec<B, T> deprecatedRecipePacketCodec() {
-		return PacketCodec.of((o1, o2) -> {
+	public static <B, T> StreamCodec<B, T> deprecatedRecipePacketCodec() {
+		return StreamCodec.ofMember((o1, o2) -> {
 			throw new IllegalStateException("Recipe packet codecs are deprecated");
 		}, o -> {
 			throw new IllegalStateException("Recipe packet codecs are deprecated");
